204a3b9b54b3e9f3354591195262d79d98f37305
Jose Pinto <pintojose.benedicto2@gmail.com>
2020-02-25 06:33:50 -0700

    Added updaing user profiles

--- backend/API/Controllers/v1/UserProfilesController.cs
+++ backend/API/Controllers/v1/UserProfilesController.cs
@@ -1,6 +1,8 @@
 using System.Threading.Tasks;
+using Application.UserProfiles.Commands;
 using Application.UserProfiles.Models;
 using Application.UserProfiles.Queries;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers.v1
@@ -11,6 +13,12 @@ namespace API.Controllers.v1
         public async Task<ActionResult<UserProfile>> GetUserProfileAsync(string username)
         {
             return await Mediator.Send(new GetUserProfile.Query { Username = username });
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Unit>> EditUserProfileAsync(EditUserProfile.Command command)
+        {
+            return await Mediator.Send(command);
         }
     }
 }
